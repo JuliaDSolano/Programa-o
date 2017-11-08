fa07a53077db2ba70580afcddbc5f08407049db8
JuliaDSolano <juliaduartesolano@gmail.com>
2017-11-08 11:17:09 -0200

    Add files via upload

---
+++ lojaIFC2info3-master/classes/conexao/Conexao.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 08/11/17
+ * Time: 10:33
+ */
+
+    require_once "../../config/config.php";
+
+    class Conexao{
+
+        public function getConexao(){
+
+            try{
+            $conexao = new PDO("mysql:host=".HOST."; dbname=".BANCO, USUARIO, SENHA);
+
+        }
+        catch (Exception $e) {
+            echo "Ocorreu um erro {$e->getMessage()} na linha {$e->getLine()}";
+
+        }
+
+    }
+
+}
+
+    //teste
+   // $conexao = new Conexao();
+    //$conexao->getConexao();
---
+++ lojaIFC2info3-master/classes/produto/Produto.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 08/11/17
+ * Time: 11:00
+ */
+require_once "../conexao/Conexao.php";
+
+class Produto
+{
+    private $codigo;
+    private $titulo;
+    private $preco;
+    private $categoria;
+    private $conexao;
+
+    /**
+     * Produto constructor.
+     * @param $codigo
+     * @param $titulo
+     * @param $preco
+     * @param $categoria
+     */
+    public function __construct($codigo, $titulo, $preco, $categoria)
+    {
+        $this->codigo = $codigo;
+        $this->titulo = $titulo;
+        $this->preco = $preco;
+        $this->categoria = $categoria;
+
+        $this->conexao = new Conexao();
+    }
+
+    public function cadastrarProduto(){
+        $sql = "INSERT INTO tb_produtos(titulo, preco, categoria) VALUES ()";
+
+    }
+}
